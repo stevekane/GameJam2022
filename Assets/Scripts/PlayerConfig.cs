7a78f62052c1a7aab09bd9a90bc6fb8b7b2d0942
Steve Kane <kanesteven@gmail.com>
2022-05-31 14:32:25 -0500

    Added rolling behavior to player

--- Assets/Scripts/PlayerConfig.cs
+++ Assets/Scripts/PlayerConfig.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Player Config", menuName = "Player/Config")]
 public class PlayerConfig : ScriptableObject {
   public float MoveSpeed = 10f;
-  public float GrappleSpeed = 100f;
-  public float GrappleFlightDuration = 2f;
+  public float MinGrappleSquareMagnitude = .5f;
+  public float RollSpeed = 20f;
+  public float RollDuration = .5f;
+  public float MaxRollRadiansPerSecond = Mathf.PI / 2;
 }
