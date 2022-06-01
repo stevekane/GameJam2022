dca3b38f3a248a23ec378362052aaad14248ecdc
Matt Perry <mpcomplete@gmail.com>
2022-06-01 15:59:36 -0400

    MobShitter shoot anim

--- Assets/Scripts/Mob.cs
+++ Assets/Scripts/Mob.cs
@@ -4,9 +4,6 @@ using UnityEngine.Events;
 public class Mob : MonoBehaviour {
   public MobConfig Config;
 
-  void Update() {
-  }
-
   public virtual void TakeDamage() {
   }
 }
--- Assets/Scripts/MobShitter.cs
+++ Assets/Scripts/MobShitter.cs
@@ -3,6 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MobShitter : Mob {
+  public enum MobShitterState { Idle, PreShoot }
+  Animator Animator;
+
+  MobShitterState State {
+    get { return (MobShitterState)Animator.GetInteger("State"); }
+    set { Animator.SetInteger("State", (int)value); }
+  }
+
+  void Start() {
+    Animator = GetComponent<Animator>();
+    State = MobShitterState.Idle;
+  }
+
+  void Update() {
+    var player = GameObject.FindObjectOfType<Player>();
+    var playerDelta = (player.transform.position - transform.position);
+    var playerInRange = playerDelta.sqrMagnitude < 100; // TODO
+
+    switch (State) {
+    case MobShitterState.Idle:
+      if (playerInRange) {
+        Debug.Log("Player in range, gonna shoot");
+        State = MobShitterState.PreShoot;
+      }
+      break;
+    case MobShitterState.PreShoot:
+      if (!playerInRange) {
+        Debug.Log("Player left range");
+        State = MobShitterState.Idle;
+      }
+      break;
+    }
+  }
+
+  public void Shoot() {
+    Debug.Log("PEW PEW");
+    State = MobShitterState.Idle;
+  }
+
   public override void TakeDamage() {
     StartCoroutine(DestroySequence());
   }
