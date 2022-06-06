e366d37db7c3d80906c00ee7ef824ec3882c921b
Matt Perry <mpcomplete@gmail.com>
2022-06-06 19:56:48 -0400

    --amend

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Player : Hero {
+public class Player : MonoBehaviour {
   public void OnRoomEntered(Room room, Door startingDoor) {
     //CharacterController.enabled = false;  // Quick hack until we have a real transition state.
     transform.position = startingDoor.transform.position - startingDoor.transform.position.normalized*2;
