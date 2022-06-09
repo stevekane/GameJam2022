bfc70d57c542645f7bdce1d114db2ba42d28e534
Matt Perry <mpcomplete@gmail.com>
2022-06-08 23:27:08 -0400

    Door dead code

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -8,4 +8,19 @@ public class Door : MonoBehaviour {
     var scene = GameObject.FindObjectOfType<SceneManager>();
     scene.EnterRoom(ConnectingRoom);
   }
+
+  //public void EnterRoom(Room room) {
+  //  var oldRoomPrefab = RoomPrefab;
+
+  //  Destroy(Room.gameObject);
+  //  RoomPrefab = room;
+  //  Room = Instantiate(RoomPrefab, transform);
+
+  //  // Put the player in front of the door that connects to our old room.
+  //  var matchingDoor = room.GetComponentsInChildren<Door>().FirstOrDefault((Door d) => d.ConnectingRoom == oldRoomPrefab);
+  //  Debug.Assert(matchingDoor != null);
+  //  var player = GameObject.FindObjectOfType<Player>();
+  //  player.OnRoomEntered(room, matchingDoor);
+  //}
+
 }
