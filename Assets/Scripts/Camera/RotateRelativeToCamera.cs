d95fc5b1860ed680fa4ce4df0760bb834c991118
Steve Kane <kanesteven@gmail.com>
2022-06-27 13:53:25 -0500

    added experimental towards-camera rotation ala Link between worlds

---
+++ Assets/Scripts/Camera/RotateRelativeToCamera.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RotateRelativeToCamera : MonoBehaviour {
+  [SerializeField]
+  float MAX_ROTATION_DEGREES = 30;
+  [SerializeField]  
+  AnimationCurve RotationByOrientation;
+
+  void LateUpdate() {
+    var camera = Camera.main;
+    var worldRotationAxis = Vector3.right;
+    var worldForwardXZ = Vector3.forward.XZ();
+    var parentForwardXZ = transform.parent.forward.XZ();
+    var dot = Vector3.Dot(worldForwardXZ,parentForwardXZ);
+    var rotationStrength = RotationByOrientation.Evaluate(dot);
+    var degrees = rotationStrength*MAX_ROTATION_DEGREES;
+    var worldRotationX = Quaternion.AngleAxis(degrees,worldRotationAxis);
+    var parentRotation = transform.parent.rotation;
+    var totalRotation = worldRotationX*parentRotation;
+    transform.rotation = totalRotation;
+  }
+}
