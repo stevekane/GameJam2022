3fdefef7d2aa091d1dc7254fc953b15c96b2c6d6
Steve Kane <kanesteven@gmail.com>
2022-07-02 15:27:07 -0500

    moved AudioSourceExtensions to common extensions file

--- Assets/Scripts/ClassExtensions.cs
+++ Assets/Scripts/ClassExtensions.cs
@@ -34,3 +34,14 @@ public static class MonoExtensions {
     }
   }
 }
+
+public static class AudioSourceExtensions {
+  public static bool PlayOptionalOneShot(this AudioSource source, AudioClip clip) {
+    if (clip) {
+      source.PlayOneShot(clip);
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
