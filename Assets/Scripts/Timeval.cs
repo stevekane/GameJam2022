efdc8721116400538b7017c2a3b6fa1353158a58
Matt Perry <mpcomplete@gmail.com>
2022-07-04 15:30:35 -0400

    Change Timeval to store tickspersecond

--- Assets/Scripts/Timeval.cs
+++ Assets/Scripts/Timeval.cs
@@ -6,14 +6,22 @@ using UnityEngine;
 public class Timeval {
   public static int FramesPerSecond = 500;
 
-  [SerializeField] int Millis = 1;
+  [SerializeField] public float Millis = 1;
+  [SerializeField] public int TicksPerSecond = 30;
 
-  public static Timeval FromMillis(int millis) {
-    return new Timeval { Millis = millis };
+  public static Timeval FromMillis(float millis, int ticksPerSecond = 30) {
+    return new Timeval { Millis = millis, TicksPerSecond = ticksPerSecond };
   }
-  public int Frames { 
-    set { Millis = value * 1000 / FramesPerSecond; } 
-    get { return Millis * FramesPerSecond / 1000; } 
+  public static Timeval FromTicks(int ticks, int ticksPerSecond) {
+    return new Timeval { Millis = (float)ticks * 1000f / ticksPerSecond, TicksPerSecond = ticksPerSecond };
+  }
+  public int Frames {
+    set { Millis = value * 1000f / FramesPerSecond; } 
+    get { return Mathf.RoundToInt(Millis * FramesPerSecond / 1000f); } 
+  }
+  public int Ticks {
+    set { Millis = value * 1000f / TicksPerSecond; }
+    get { return Mathf.RoundToInt(Millis * TicksPerSecond / 1000f); }
   }
   public float Seconds { 
     get { return Millis * .0001f; } 
@@ -24,15 +32,32 @@ public class Timeval {
 public class TimevalDrawer : PropertyDrawer {
   public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
     EditorGUI.BeginProperty(position, label, property);
-    position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+    Rect p = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+    var indent = EditorGUI.indentLevel;
+    EditorGUI.indentLevel = 0;
 
     EditorGUI.BeginChangeCheck();
     var millisProp = property.FindPropertyRelative("Millis");
-    var timeval = Timeval.FromMillis(millisProp.intValue);
-    // TODO: should we input in frames, millis, or seconds? Can use PropertyAttribute to customize per-object.
-    var newValue = EditorGUI.IntField(position, $"Millis (frames={timeval.Frames})", millisProp.intValue);
-    if (EditorGUI.EndChangeCheck())
-      millisProp.intValue = newValue;
+    var tpsProp = property.FindPropertyRelative("TicksPerSecond");
+    var timeval = Timeval.FromMillis(millisProp.floatValue, tpsProp.intValue);
+
+    p.width /= 2;
+    var labels = new[] { new GUIContent("Ticks"), new GUIContent("tps") };
+    EditorGUIUtility.labelWidth = EditorStyles.label.CalcSize(labels[0]).x;
+    var newTicks = EditorGUI.IntField(p, labels[0], timeval.Ticks);
+    p.x += p.width;
+    EditorGUIUtility.labelWidth = EditorStyles.label.CalcSize(labels[1]).x;
+    var newTps = EditorGUI.IntField(p, labels[1], tpsProp.intValue);
+
+    if (EditorGUI.EndChangeCheck()) {
+      timeval = Timeval.FromTicks(newTicks, newTps);
+      Debug.Log($"Millis change {millisProp.floatValue} => {timeval.Millis}");
+      millisProp.floatValue = timeval.Millis;
+      tpsProp.intValue = timeval.TicksPerSecond;
+    }
+
+    EditorGUI.indentLevel = indent;
 
     EditorGUI.EndProperty();
   }
