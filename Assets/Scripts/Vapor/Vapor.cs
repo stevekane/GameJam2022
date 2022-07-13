24fabfd6e553744926ff70344a4ae9883795796e
Steve Kane <kanesteven@gmail.com>
2022-07-12 22:28:36 -0500

    added shiutty dash to Vapor

--- Assets/Scripts/Vapor/Vapor.cs
+++ Assets/Scripts/Vapor/Vapor.cs
@@ -3,16 +3,6 @@ using UnityEngine;
 enum Motion { Base, Dashing }
 
 public class Vapor : MonoBehaviour {
-  public static float MOVE_SPEED = 15f;
-  public static float TURN_SPEED = 720f;
-
-  [SerializeField] Attacker Attacker;
-  [SerializeField] Cannon Cannon;
-  [SerializeField] Pushable Pushable;
-  [SerializeField] CharacterController Controller;
-
-  Motion Motion;
-
   public static Quaternion RotationFromInputs(Transform t, float speed, Action action, float dt) {
     var desiredForward = 
       action.Aim.XZ.TryGetDirection() ??
@@ -24,20 +14,36 @@ public class Vapor : MonoBehaviour {
     return Quaternion.RotateTowards(currentRotation, desiredRotation, degrees);
   }
 
+  public static Vector3 HeadingFromInputs(Transform t, Action action) {
+    var heading = 
+      action.Move.XZ.TryGetDirection() ??
+      action.Aim.XZ.TryGetDirection() ??
+      t.forward;
+    return heading;
+  }
+
   public static Vector3 VelocityFromMove(Action action, float speed) {
     return speed*action.Move.XZ;
   }
 
+  [SerializeField] float MOVE_SPEED;
+  [SerializeField] float TURN_SPEED;
+  [SerializeField] float DASH_SPEED;
+  [SerializeField] Timeval DashDuration;
+  [SerializeField] Attacker Attacker;
+  [SerializeField] Cannon Cannon;
+  [SerializeField] Pushable Pushable;
+  [SerializeField] CharacterController Controller;
+
+  Motion Motion;
+  Vector3 Velocity;
+  Vector3 DashHeading;
+  float DashFramesRemaining;
+
   void FixedUpdate() {
     var dt = Time.fixedDeltaTime;
     var action = Inputs.Action;
-    var moveVelocity = VelocityFromMove(action, MOVE_SPEED);
-    var pushVelocity = Pushable.Impulse;
-    var velocity = moveVelocity;//+pushVelocity;
-
-    Controller.Move(dt*velocity);
-
-    transform.rotation = RotationFromInputs(transform, TURN_SPEED, action, dt);
+    
     if (action.Jump.JustDown) {
       Cannon.DepressTrigger();
     } else if (action.Jump.Down) {
@@ -45,5 +51,22 @@ public class Vapor : MonoBehaviour {
     } else if (action.Jump.JustUp) {
       Cannon.ReleaseTrigger();
     }
+
+    if (action.Hit.JustDown && Motion == Motion.Base) {
+      DashHeading = HeadingFromInputs(transform, action);
+      DashFramesRemaining = DashDuration.Frames;
+      Motion = Motion.Dashing;
+    } else if (Motion == Motion.Dashing && DashFramesRemaining <= 0) {
+      Motion = Motion.Base;
+    }
+
+    if (Motion == Motion.Base) {
+      Velocity = VelocityFromMove(action, MOVE_SPEED)+Pushable.Impulse;
+      Controller.Move(dt*Velocity);
+    } else if (Motion == Motion.Dashing) {
+      DashFramesRemaining--;
+      Controller.Move(dt*DASH_SPEED*DashHeading);
+    }
+    transform.rotation = RotationFromInputs(transform, TURN_SPEED, action, dt);
   }
 }
