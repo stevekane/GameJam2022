ae60e86237f3fe751c1aaf5b59cf5756bd56cc4c
Steve Kane <kanesteven@gmail.com>
2022-07-12 23:04:57 -0500

    AttackConfigEditor now only displays the animationpreview when an animation clip is provided instead of just shitting the bed

--- Assets/Scripts/Editor/AttackConfigEditor.cs
+++ Assets/Scripts/Editor/AttackConfigEditor.cs
@@ -9,27 +9,28 @@ public class AttackConfigEditor : Editor {
   float Time;
 
   public override void OnInspectorGUI() {
-    if (!ClipEditor) {
-      ClipEditor = Editor.CreateEditor(Preview);
-      ClipEditor.HasPreviewGUI();
-    }
     var attackConfig = (AttackConfig)target;
     var clip = attackConfig.Clip;
-    var frame = (int)(clip.frameRate * Time);
 
-    EditorGUILayout.BeginVertical();
-    GUILayout.Label($"Frame: {frame}");
-    Time = EditorGUILayout.Slider(Time, 0, clip.length);
-    AnimationMode.StartAnimationMode();
-    AnimationMode.BeginSampling();
-    AnimationMode.SampleAnimationClip(Preview, clip, Time);
-    AnimationMode.EndSampling();
-    AnimationMode.StopAnimationMode();
-    ClipEditor.OnPreviewSettings();
-    ClipEditor.OnInteractivePreviewGUI(GUILayoutUtility.GetRect(256,256), EditorStyles.whiteLabel);
-    ClipEditor.ReloadPreviewInstances();
-    EditorGUILayout.EndVertical();
+    if (clip) {
+      if (!ClipEditor) {
+        ClipEditor = Editor.CreateEditor(Preview);
+        ClipEditor.HasPreviewGUI();
+      }
+      var frame = (int)(clip.frameRate * Time);
+      EditorGUILayout.BeginVertical();
+      GUILayout.Label($"Frame: {frame}");
+      Time = EditorGUILayout.Slider(Time, 0, clip.length);
+      AnimationMode.StartAnimationMode();
+      AnimationMode.BeginSampling();
+      AnimationMode.SampleAnimationClip(Preview, clip, Time);
+      AnimationMode.EndSampling();
+      AnimationMode.StopAnimationMode();
+      ClipEditor.OnPreviewSettings();
+      ClipEditor.OnInteractivePreviewGUI(GUILayoutUtility.GetRect(256,256), EditorStyles.whiteLabel);
+      ClipEditor.ReloadPreviewInstances();
+      EditorGUILayout.EndVertical();
+    }
     base.OnInspectorGUI();
-
   }
 }
