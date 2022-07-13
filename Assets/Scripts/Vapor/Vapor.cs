47ea74f4097a45b5d4d2a58e230c712dcb8b26ac
Steve Kane <kanesteven@gmail.com>
2022-07-13 18:36:34 -0500

    added light melee attacks to Vapor

--- Assets/Scripts/Vapor/Vapor.cs
+++ Assets/Scripts/Vapor/Vapor.cs
@@ -32,11 +32,13 @@ public class Vapor : MonoBehaviour {
   [SerializeField] Cannon Cannon;
   [SerializeField] Pushable Pushable;
   [SerializeField] CharacterController Controller;
+  [SerializeField] Animator Animator;
 
   Motion Motion;
   Vector3 Velocity;
   Vector3 DashHeading;
   float DashFramesRemaining;
+  int PunchCycleIndex;
 
   void FixedUpdate() {
     var dt = Time.fixedDeltaTime;
@@ -57,6 +59,17 @@ public class Vapor : MonoBehaviour {
     } else if (Motion == Motion.Dashing && DashFramesRemaining <= 0) {
       Motion = Motion.Base;
     }
+
+    if (action.R1.JustDown && !Attacker.IsAttacking) {
+      PunchCycleIndex = PunchCycleIndex <= 0 ? 1 : 0;
+      Attacker.StartAttack(0);
+    }
+
+    Attacker.Step(dt);
+    Animator.SetBool("Attacking", Attacker.IsAttacking);
+    Animator.SetInteger("AttackIndex", Attacker.AttackIndex);
+    Animator.SetInteger("AttackSequenceIndex", PunchCycleIndex);
+    Animator.SetFloat("AttackSpeed", Attacker.AttackSpeed);
 
     if (Motion == Motion.Base) {
       Velocity = VelocityFromMove(action, MOVE_SPEED)+Pushable.Impulse;
