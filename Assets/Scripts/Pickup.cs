829eda4a540dc6be15e93f42063e04d2e81f233a
Matt Perry <mpcomplete@gmail.com>
2022-10-14 13:33:04 -0400

    Only player can pick up upgrades

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Pickup : MonoBehaviour {
   public Upgrade Upgrade;
   void OnTriggerEnter(Collider c) {
-    if (c.TryGetComponent(out Upgrades us))
+    if (c.GetComponent<Player>() && c.TryGetComponent(out Upgrades us))
       OnPickup(us);
   }
   void OnPickup(Upgrades us) {
