610df7bf85ea1de2f1773a6730b73c2f79f9b910
Steve Kane <kanesteven@gmail.com>
2022-10-14 16:06:46 -0500

    overhauled the way game manager configures and dispatches encounters to make it more parameterized and flexible

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,9 +1,65 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+
+/*
+An encounter is simply a program with various continuations.
+
+The current program is to spawn a mob every so often at some spawning location.
+
+The program we want now has a few parts:
+
+  Spawning happens in waves
+    Concurrent
+    Staggered
+
+  Each wave has a continuation condition
+    All defeated
+    Time elapsed
+    Total mobs remaining
+
+  A wave is generated from a seed, a budget, unit prices, mob strategy and spawning strategy
+
+  MobStrategy is an algorithm for consuming the budget
+    Expensive units
+    Cheap units
+    Alternation between expensive and cheap units
+    Same unit
+    Unique units
+
+  SpawnStrategy is an algorithm for selecting spawn locations for a list of mobs
+    Close together
+    Far apart
+    Evenly spaced
+    Random
+    Single Spawn
+
+  Seed : Int
+  Budget : Int
+  Cost : Int
+  SpawnOption : (Unit,Cost)
+  MobStrategy : Set<SpawnOption> -> [Unit]
+  SpawnStrategy : [Unit] -> Set<Transform> -> [(Unit,Transform)]
+
+  A spawning process may
+    send spawn requests to the MobManager
+    play vfx/sfx
+
+  Loop over waves
+    calculate mobstrategy to get list of units
+    calculate spawnstrategy to get spawnrequests
+    play spawn process
+    wait for wave condition (initially passage of time)
+*/
+
+public enum MobStrategy { Base }
+public enum SpawnStrategy { Base }
+public enum SpawnProcess { Staggered } // TODO: needs parameters for wave size and spawn period
+public enum NextWaveCondition { Time } // TODO: needs paramater for duration and other params
 
 [Serializable]
 public struct Effect {
@@ -12,10 +68,34 @@ public struct Effect {
 }
 
 [Serializable]
-public struct MobSpawnConfig {
+public struct SpawnConfig {
+  public int Cost;
   public Effect PreviewEffect;
   public Effect SpawnEffect;
   public GameObject Mob;
+}
+
+[Serializable]
+public struct SpawnRequest {
+  public Transform transform;
+  public SpawnConfig config;
+}
+
+[Serializable]
+public struct Wave {
+  public int Budget;
+  public MobStrategy MobStrategy;
+  public SpawnStrategy SpawnStrategy;
+  public SpawnProcess SpawnProcess;
+  public NextWaveCondition NextWaveCondition;
+}
+
+[Serializable]
+public struct Encounter {
+  public int Seed;
+  public List<SpawnConfig> SpawnConfigs;
+  public List<Transform> SpawnPoints;
+  public List<Wave> Waves;
 }
 
 public class GameManager : MonoBehaviour {
@@ -38,9 +118,11 @@ public class GameManager : MonoBehaviour {
 
   [Header("Mobs")]
   public Transform[] MobSpawns;
-  public MobSpawnConfig[] MobSpawnConfigs;
+  public SpawnConfig[] MobSpawnConfigs;
+  public Encounter DefaultEncounter;
 
   GameObject Player;
+  List<GameObject> Mobs;
 
   void Awake() {
     if (Instance) {
@@ -69,6 +151,21 @@ public class GameManager : MonoBehaviour {
     return !Player || Player.GetComponent<Attributes>().GetValue(AttributeTag.Health, 0f) <= 0;
   }
 
+  List<SpawnConfig> BaseMobs(List<SpawnConfig> spawnConfigs) {
+    return spawnConfigs;
+  }
+
+  List<SpawnRequest> BaseSpawns(List<SpawnConfig> spawnConfigs, List<Transform> spawnPoints) {
+    var spawnRequests = new List<SpawnRequest>(spawnConfigs.Count);
+    for (var i = 0; i < spawnConfigs.Count; i++) {
+      spawnRequests.Add(new SpawnRequest {
+        config = spawnConfigs[i],
+        transform = spawnPoints[i%spawnPoints.Count]
+      });
+    }
+    return spawnRequests;
+  }
+
   IEnumerator Countdown(
   Action<int> f,
   int seconds) {
@@ -78,23 +175,52 @@ public class GameManager : MonoBehaviour {
     }
   }
 
-  IEnumerator SpawnMob(MobSpawnConfig config, Transform targetTransform) {
-    var p = targetTransform.position;
-    var r = targetTransform.rotation;
-    VFXManager.Instance.SpawnEffect(config.PreviewEffect, p, r);
-    yield return new WaitForSeconds(config.PreviewEffect.Duration);
-    VFXManager.Instance.SpawnEffect(config.SpawnEffect, p, r);
-    Instantiate(config.Mob, p, r);
+  IEnumerator SpawnMob(SpawnRequest sr) {
+    var p = sr.transform.position;
+    var r = sr.transform.rotation;
+    VFXManager.Instance.SpawnEffect(sr.config.PreviewEffect, p, r);
+    yield return new WaitForSeconds(sr.config.PreviewEffect.Duration);
+    VFXManager.Instance.SpawnEffect(sr.config.SpawnEffect, p, r);
+    Mobs.Add(Instantiate(sr.config.Mob, p, r));
   }
 
-  IEnumerator Forever(Func<IEnumerator> mkProc) {
-    while (true) {
-      yield return mkProc();
+  IEnumerator SpawnConcurrent(List<SpawnRequest> spawnRequests) {
+    foreach (var sr in spawnRequests) {
+      StartCoroutine(SpawnMob(sr));
+    }
+    yield return null;
+  }
+
+  IEnumerator SpawnStaggered(List<SpawnRequest> spawnRequests, int groupSize, float seconds) {
+    for (var i = 0; i < groupSize; i++) {
+      if (i % groupSize == 0) {
+        yield return new WaitForSeconds(seconds);
+      } else {
+        yield return StartCoroutine(SpawnMob(spawnRequests[i]));
+      }
+    }
+  }
+
+  IEnumerator RunEncounter(Encounter encounter) {
+    foreach (var wave in encounter.Waves) {
+      Debug.Log("Spawning a wave");
+      var mobs = wave.MobStrategy switch {
+        _ => BaseMobs(encounter.SpawnConfigs)
+      };
+      var spawns = wave.SpawnStrategy switch {
+        _ => BaseSpawns(mobs, encounter.SpawnPoints)
+      };
+      Debug.Log($"{mobs.Count} mobs spawning at {spawns.Count} locations");
+      yield return StartCoroutine(wave.SpawnProcess switch {
+        _ => SpawnConcurrent(spawns)
+      });
+      yield return wave.NextWaveCondition switch {
+        _ => new WaitForSeconds(5) // TODO: hardcoded.. need to decide where this parameter lives
+      };
     }
   }
 
   IEnumerator Start() {
-    // !(spawn(player).countdown(3).(gameover.reload+!time.spawn(enemy)))
     while (true) {
       // Spawn and configure the player
       var playerSpawn = PlayerSpawns[0];
@@ -110,13 +236,13 @@ public class GameManager : MonoBehaviour {
       SetPlayerInputsEnabled(Player, isEnabled: true);
       // Exit pre-game countdown
 
-      var i = 0;
-      var j = 0;
+      // Start the encounter
+      var encounter = StartCoroutine(RunEncounter(DefaultEncounter));
       while (!GameOver()) {
-        yield return StartCoroutine(SpawnMob(MobSpawnConfigs[j], MobSpawns[i]));
-        i = (i+1)%MobSpawns.Length;
-        j = (j+1)%MobSpawnConfigs.Length;
+        yield return null;
       }
+      StopCoroutine(encounter);
+      // Stop encounter
 
       // Cleanup references and reload the scene
       yield return StartCoroutine(Await(SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name)));
