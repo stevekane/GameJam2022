249d132821689a028675a0aae9dcfdff9c8fbc9e
Steve Kane <kanesteven@gmail.com>
2022-10-20 16:28:30 -0500

    revamped WaveEncounter to be very dumb and simple to configure. Uses the balls and sand algorithm for filling the budget

--- Assets/Scripts/Encounters/WaveEncounter.cs
+++ Assets/Scripts/Encounters/WaveEncounter.cs
@@ -1,85 +1,71 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public enum MobStrategy { Base }
-public enum SpawnStrategy { Base }
-public enum SpawnProcess { Staggered, Concurrent } // TODO: needs parameters for wave size and spawn period
-public enum NextWaveCondition { Time } // TODO: needs paramater for duration and other params
+[Serializable]
+public struct MobCost {
+  public GameObject Mob;
+  public int Cost;
+}
 
 [Serializable]
-public struct Wave {
-  public int Budget;
-  public MobStrategy MobStrategy;
-  public SpawnStrategy SpawnStrategy;
-  public SpawnProcess SpawnProcess;
-  public NextWaveCondition NextWaveCondition;
+public struct WaveConfig {
+  public Transform[] SpawnPoints;
+  public SpawnConfig[] SpawnConfigs;
+  public List<MobCost> Costs;
 }
 
 public class WaveEncounter : Encounter {
-  public List<SpawnConfig> SpawnConfigs;
-  public List<Transform> SpawnPoints;
-  public List<Wave> Waves;
+  public int InitialCost;
+  public int CostPerWave;
+  public int TotalWaveCount;
+  public Timeval WavePeriod;
+  public WaveConfig Config;
+
+  int Cost(List<MobCost> mobCosts, GameObject mob, int defaultCost = 1) {
+    foreach (var mobCost in mobCosts) {
+      if (mobCost.Mob == mob) {
+        return mobCost.Cost;
+      }
+    }
+    return defaultCost;
+  }
+
+  IEnumerable<SpawnRequest> Wave(int budget, WaveConfig config) {
+    var candidates = (config.SpawnConfigs.Clone() as SpawnConfig[]).OrderByDescending(sc => Cost(config.Costs, sc.Mob));
+    var transforms = (config.SpawnPoints.Clone() as Transform[]);
+    var price = 0;
+
+    foreach (var t in transforms) {
+      foreach (var c in candidates) {
+        var cost = Cost(config.Costs, c.Mob);
+        var nextPrice = cost+price;
+        if (nextPrice <= budget) {
+          price = nextPrice;
+          yield return new SpawnRequest { config = c, transform = t };
+          break;
+        }
+      }
+    }
+  }
 
   IEnumerator SpawnMob(SpawnRequest sr) {
     var p = sr.transform.position;
     var r = sr.transform.rotation;
     VFXManager.Instance.SpawnEffect(sr.config.PreviewEffect, p, r);
-    yield return new WaitForSeconds(sr.config.PreviewEffect.Duration.Seconds);
+    yield return new WaitForSeconds(sr.config.PreviewEffect.Duration.Frames);
     VFXManager.Instance.SpawnEffect(sr.config.SpawnEffect, p, r);
     Instantiate(sr.config.Mob, p, r);
   }
 
-  IEnumerator SpawnConcurrent(List<SpawnRequest> spawnRequests) {
-    foreach (var sr in spawnRequests) {
-      StartCoroutine(SpawnMob(sr));
-    }
-    yield return null;
-  }
-
-  IEnumerator SpawnStaggered(List<SpawnRequest> spawnRequests, int groupSize, float seconds) {
-    var batch = new List<SpawnRequest>();
-    for (var i = 0; i < spawnRequests.Count; i++) {
-      batch.Add(spawnRequests[i]);
-      if (batch.Count == groupSize) {
-        StartCoroutine(SpawnConcurrent(batch));
-        batch.Clear();
-        yield return new WaitForSeconds(seconds);
-      }
-    }
-  }
-
-  List<SpawnConfig> BaseMobs(List<SpawnConfig> spawnConfigs) {
-    return spawnConfigs;
-  }
-
-  List<SpawnRequest> BaseSpawns(List<SpawnConfig> spawnConfigs, List<Transform> spawnPoints) {
-    var spawnRequests = new List<SpawnRequest>(spawnConfigs.Count);
-    for (var i = 0; i < spawnConfigs.Count; i++) {
-      spawnRequests.Add(new SpawnRequest {
-        config = spawnConfigs[i],
-        transform = spawnPoints[i%spawnPoints.Count]
-      });
-    }
-    return spawnRequests;
-  }
-
   public override IEnumerator Run() {
-    foreach (var wave in Waves) {
-      var mobs = wave.MobStrategy switch {
-        _ => BaseMobs(SpawnConfigs)
-      };
-      var spawns = wave.SpawnStrategy switch {
-        _ => BaseSpawns(mobs, SpawnPoints)
-      };
-      yield return StartCoroutine(wave.SpawnProcess switch {
-        SpawnProcess.Staggered => SpawnStaggered(spawns, 1, 2), // TODO: hardcoded.. need to decide where these params live
-        SpawnProcess.Concurrent => SpawnConcurrent(spawns)
-      });
-      yield return wave.NextWaveCondition switch {
-        _ => new WaitForSeconds(5) // TODO: hardcoded.. need to decide where this parameter lives
-      };
+    for (var waveNumber = 0; waveNumber < TotalWaveCount; waveNumber++) {
+      yield return Fiber.Wait(WavePeriod.Frames);
+      Wave(InitialCost+CostPerWave*waveNumber, Config)
+      .Select(sr => new Fiber(SpawnMob(sr)))
+      .ForEach(Bundle.StartRoutine);
     }
   }
 }
