d30360f8b0ca1b76042e888e8f434ba5d5fef77d
Matt Perry <mpcomplete@gmail.com>
2022-10-20 16:37:26 -0400

    Upgrades have associated cost, currently unused

--- Assets/Scripts/UpgradeAbilityList.cs
+++ Assets/Scripts/UpgradeAbilityList.cs
@@ -7,7 +7,12 @@ public class UpgradeAbilityListData : UpgradeData {
 
 [CreateAssetMenu(fileName = "Upgrade", menuName = "Upgrade/Ability")]
 public class UpgradeAbilityList : Upgrade {
-  public AbilityTag[] AbilityTags;
+  [System.Serializable]
+  public class Level {
+    public AbilityTag AbilityTag;
+    public int Cost = 0;
+  }
+  public Level[] Levels;
   UpgradeAbilityListData GetData(Upgrades us) => us.FindUpgrade(ud => ud.Upgrade == this) as UpgradeAbilityListData;
   public override void Add(Upgrades us) {
     if (GetData(us) is var ud && ud != null) {
@@ -17,5 +22,6 @@ public class UpgradeAbilityList : Upgrade {
       us.AddUpgrade(new UpgradeAbilityListData() { Upgrade = this, CurrentLevel = 0 });
     }
   }
-  public override void Apply(Upgrades us) => us.AbilityTags.AddFlags(AbilityTags[GetData(us).CurrentLevel]);
+  public override void Apply(Upgrades us) => us.AbilityTags.AddFlags(Levels[GetData(us).CurrentLevel].AbilityTag);
+  public override int GetCost(Upgrades us) => Levels.TryGetIndex(GetData(us).CurrentLevel+1, out Level lvl) ? lvl.Cost : int.MaxValue;
 }
