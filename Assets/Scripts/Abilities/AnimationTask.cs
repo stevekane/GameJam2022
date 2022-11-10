1e99d832bb8050656bf784e5c79029e8dfdd2a17
Steve Kane <kanesteven@gmail.com>
2022-11-10 15:22:39 -0600

    added base animation for sniper along with Animator controller

--- Assets/Scripts/Abilities/AnimationTask.cs
+++ Assets/Scripts/Abilities/AnimationTask.cs
@@ -15,32 +15,34 @@ public class AnimationTask : IEnumerator, IStoppable {
   AnimationClipPlayable ClipPlayable;
   AnimationPlayableOutput Output;
   PlayableGraph Graph;
-  double Duration;
   int EventHead;
 
   public FrameEventTimeline FrameEventTimeline;
   public Action<FrameEvent> OnFrameEvent;
 
   public AnimationTask(Animator animator, AnimationClip clip) {
-    IsRunning = true;
     EventHead = 0;
     Animator = animator;
     Graph = PlayableGraph.Create();
-    Duration = clip.isLooping ? double.MaxValue : clip.length;
     ClipPlayable = AnimationClipPlayable.Create(Graph, clip);
-    ClipPlayable.SetDuration(Duration);
+    ClipPlayable.SetDuration(clip.length);
     Output = AnimationPlayableOutput.Create(Graph, "AnimationTask", Animator);
     Output.SetSourcePlayable(ClipPlayable);
     Graph.SetTimeUpdateMode(DirectorUpdateMode.GameTime);
     Graph.Play();
+    IsRunning = true;
   }
   ~AnimationTask() {
+    Output.SetTarget(null);
     ClipPlayable.Destroy();
+    Graph.Stop();
     Graph.Destroy();
     IsRunning = false;
   }
   public void Stop() {
+    Output.SetTarget(null);
     ClipPlayable.Destroy();
+    Graph.Stop();
     Graph.Destroy();
     IsRunning = false;
   }
@@ -53,7 +55,7 @@ public class AnimationTask : IEnumerator, IStoppable {
         var clip = ClipPlayable.GetAnimationClip();
         var frames = clip.length*clip.frameRate+1;
         var time = ClipPlayable.GetTime();
-        var interpolant = (float)(time/Duration);
+        var interpolant = (float)(time/clip.length);
         var newHead = (int)Mathf.Lerp(0, frames, interpolant);
         var oldHead = EventHead;
         for (var i = oldHead; i < newHead; i++) {
