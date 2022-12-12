6e460b8b2efaa655172819db05be1ebcd48cd9e7
Matt Perry <mpcomplete@gmail.com>
2022-12-12 15:39:51 -0500

    Bombard uses a Task sequence, with proper HitParams

--- Assets/Characters/PigMoss/TargetedStrike.cs
+++ Assets/Characters/PigMoss/TargetedStrike.cs
@@ -12,20 +12,4 @@ public class TargetedStrike : MonoBehaviour {
   public AnimationCurve Alpha = AnimationCurve.Constant(0,1,1);
   public Timeval Duration = Timeval.FromSeconds(1);
   public DecalProjector Projector;
-
-  IEnumerator Start() {
-    const float MAX_PROJECTION_DEPTH = 10;
-    var totalTicks = Duration.Ticks;
-    for (var tick = 0; tick <= totalTicks; tick++) {
-      var interpolant = (float)tick/(float)totalTicks;
-      var radius = Mathf.Lerp(MinRadius, MaxRadius, Radius.Evaluate(interpolant));
-      var alpha = Mathf.Lerp(MinAlpha, MaxAlpha, Alpha.Evaluate(interpolant));
-      Projector.fadeFactor = alpha;
-      Projector.size = new Vector3(radius, radius, MAX_PROJECTION_DEPTH);
-      yield return null;
-    }
-    var explosion = Instantiate(SpawnPrefab, transform.position, transform.rotation);
-    Destroy(explosion, 5);
-    Destroy(gameObject);
-  }
 }
