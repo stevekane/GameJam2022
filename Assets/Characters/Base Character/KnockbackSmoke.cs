067f887d8b8775120ac3230a63cd08a4acbbf133
Steve Kane <kanesteven@gmail.com>
2022-12-12 17:04:24 -0600

    removed unneeded charactoer controller and emit params from kb smoke

--- Assets/Characters/Base Character/KnockbackSmoke.cs
+++ Assets/Characters/Base Character/KnockbackSmoke.cs
@@ -2,14 +2,12 @@ using UnityEngine;
 
 public class KnockbackSmoke : MonoBehaviour {
   [SerializeField] Status Status;
-  [SerializeField] CharacterController Controller;
   [SerializeField] ParticleSystem ParticleSystem;
   [SerializeField] float ParticleSize = 1f;
 
   void FixedUpdate() {
     if (Status.IsHurt) {
-      var emitParams = new ParticleSystem.EmitParams {};
-      ParticleSystem.Emit(emitParams, 1);
+      ParticleSystem.Emit(1);
     }
   }
 }
