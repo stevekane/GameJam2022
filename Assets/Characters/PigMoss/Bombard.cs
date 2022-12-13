58b9ff0e82660338eaa712b12d7590077ad574c9
Matt Perry <mpcomplete@gmail.com>
2022-12-13 17:58:44 -0500

    Small cleanup in Pigmoss Bombard

--- Assets/Characters/PigMoss/Bombard.cs
+++ Assets/Characters/PigMoss/Bombard.cs
@@ -28,7 +28,7 @@ namespace PigMoss {
         Animator.SetBool("Extended", true);
         await scope.Delay(Windup);
         foreach (var launchSite in LaunchSites) {
-          TaskScope.Start(new Task(async () => await LaunchMissle(HitConfig, Attributes.serialized, Attributes.gameObject, MissilePrefab, launchSite, Radius, ShotClip, ShotEffect)));
+          GameManager.Instance.GlobalScope.Start(s => LaunchMissle(s, HitConfig, Attributes.serialized, Attributes.gameObject, MissilePrefab, launchSite, Radius, ShotClip, ShotEffect));
           await scope.Delay(ShotPeriod);
         }
         SFXManager.Instance.TryPlayOneShot(RecoveryClip);
@@ -39,44 +39,34 @@ namespace PigMoss {
       }
     }
 
-    static async Task LaunchMissle(HitConfig hitConfig, Attributes.Serialized attributes, GameObject attacker, Missile misslePrefab, Transform launchSite, float radius, AudioClip sfx, GameObject vfx) {
-      using TaskScope scope = new(GameManager.Instance.GlobalScope);
-      Missile missile = null;
-      TargetedStrike payload = null;
+    static async Task LaunchMissle(TaskScope scope, HitConfig hitConfig, Attributes.Serialized attributes, GameObject attacker, Missile misslePrefab, Transform launchSite, float radius, AudioClip sfx, GameObject vfx) {
+      // Launch missile.
+      var missile = Instantiate(misslePrefab, launchSite.position, launchSite.rotation);
+      SFXManager.Instance.TryPlayOneShot(sfx);
+      VFXManager.Instance.TrySpawnEffect(vfx, launchSite.position);
+      await scope.Delay(missile.Duration);
 
-      try {
-        // Launch missile.
-        missile = Instantiate(misslePrefab, launchSite.position, launchSite.rotation);
-        SFXManager.Instance.TryPlayOneShot(sfx);
-        VFXManager.Instance.TrySpawnEffect(vfx, launchSite.position);
-        await scope.Delay(missile.Duration);
+      // Spawn payload.
+      var target = radius*UnityEngine.Random.onUnitSphere.XZ();
+      var payload = Instantiate(missile.PayloadPrefab, target, Quaternion.identity).GetComponent<TargetedStrike>();
+      Destroy(missile.gameObject);
 
-        // Spawn payload.
-        var target = radius*UnityEngine.Random.onUnitSphere.XZ();
-        payload = Instantiate(missile.PayloadPrefab, target, Quaternion.identity).GetComponent<TargetedStrike>();
-        Destroy(missile.gameObject);
-
-        const float MAX_PROJECTION_DEPTH = 10;
-        var totalTicks = payload.Duration.Ticks;
-        for (var tick = 0; tick <= totalTicks; tick++) {
-          var interpolant = (float)tick/(float)totalTicks;
-          var pradius = Mathf.Lerp(payload.MinRadius, payload.MaxRadius, payload.Radius.Evaluate(interpolant));
-          var alpha = Mathf.Lerp(payload.MinAlpha, payload.MaxAlpha, payload.Alpha.Evaluate(interpolant));
-          payload.Projector.fadeFactor = alpha;
-          payload.Projector.size = new Vector3(pradius, pradius, MAX_PROJECTION_DEPTH);
-          await scope.Tick();
-        }
-
-        // Explode.
-        var explosion = Instantiate(payload.SpawnPrefab, payload.transform.position, payload.transform.rotation).GetComponent<Explosion>();
-        explosion.HitParams = new HitParams(hitConfig, attributes, attacker, explosion.gameObject);
-        Destroy(explosion.gameObject, 5);
-        Destroy(payload.gameObject);
-      } catch (OperationCanceledException) {
-      } finally {
-        if (missile) Destroy(missile.gameObject);
-        if (payload) Destroy(payload.gameObject);
+      const float MAX_PROJECTION_DEPTH = 10;
+      var totalTicks = payload.Duration.Ticks;
+      for (var tick = 0; tick <= totalTicks; tick++) {
+        var interpolant = (float)tick/(float)totalTicks;
+        var pradius = Mathf.Lerp(payload.MinRadius, payload.MaxRadius, payload.Radius.Evaluate(interpolant));
+        var alpha = Mathf.Lerp(payload.MinAlpha, payload.MaxAlpha, payload.Alpha.Evaluate(interpolant));
+        payload.Projector.fadeFactor = alpha;
+        payload.Projector.size = new Vector3(pradius, pradius, MAX_PROJECTION_DEPTH);
+        await scope.Tick();
       }
+
+      // Explode.
+      var explosion = Instantiate(payload.SpawnPrefab, payload.transform.position, payload.transform.rotation).GetComponent<Explosion>();
+      explosion.HitParams = new HitParams(hitConfig, attributes, attacker, explosion.gameObject);
+      Destroy(explosion.gameObject, 5);
+      Destroy(payload.gameObject);
     }
   }
 }
