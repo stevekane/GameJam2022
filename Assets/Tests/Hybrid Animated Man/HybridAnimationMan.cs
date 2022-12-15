95fd984952fdf3319ea2f187d3a4e992fb3e14bc
Steve Kane <kanesteven@gmail.com>
2022-12-14 23:32:00 -0600

    ham no longer has weird script to stun himself

--- Assets/Tests/Hybrid Animated Man/HybridAnimationMan.cs
+++ Assets/Tests/Hybrid Animated Man/HybridAnimationMan.cs
@@ -1,22 +1,4 @@
 using System.Collections;
 using UnityEngine;
 
-public class HybridAnimationMan : MonoBehaviour {
-  [SerializeField] Status Status;
-  [SerializeField] Timeval Duration = Timeval.FromMillis(500);
-
-  void Awake() => InputManager.Instance.ButtonEvent(ButtonCode.West, ButtonPressType.JustDown).Listen(StunSelf);
-  void StunSelf() => StartCoroutine(StunSelfRoutine());
-
-  IEnumerator StunSelfRoutine() {
-    Debug.Log("Stunning self");
-    var effect = new InlineEffect(s => {
-      s.IsHurt = true;
-    });
-    Status.Add(effect);
-    for (var i = 0; i < Duration.Ticks; i++) {
-      yield return new WaitForFixedUpdate();
-    }
-    Status.Remove(effect);
-  }
-}
+public class HybridAnimationMan : MonoBehaviour {}
