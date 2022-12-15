ca94cc8f618bf7874f31d57df89d6736d3d2b792
Matt Perry <mpcomplete@gmail.com>
2022-12-15 01:01:54 -0500

    Remove Hollow Knight

--- Assets/Tests/Hollow Knight/Destructible.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Destructible : MonoBehaviour {
-  public GameObject Debris;
-  void OnTriggerEnter2D(Collider2D c) {
-    Destroy(gameObject);
-    Destroy(Instantiate(Debris, transform.position, transform.rotation), 5);
-  }
-}
--- Assets/Tests/Hollow Knight/InstantKill.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class InstantKill : MonoBehaviour {
-  void OnTriggerEnter2D(Collider2D c) {
-    if (c.GetComponent<HollowKnight>()) {
-      Destroy(c.gameObject);
-    }
-  }
-}
