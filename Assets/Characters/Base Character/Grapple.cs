f9e1ca17fde7988a45ef223bf70c9120ca61f08b
Steve Kane <kanesteven@gmail.com>
2023-01-09 09:54:35 -0600

    minor tweaks to grappling setup

--- Assets/Characters/Base Character/Grapple.cs
+++ Assets/Characters/Base Character/Grapple.cs
@@ -1,6 +1,45 @@
 using System.Threading.Tasks;
 using UnityEngine;
 
+/*
+Targeting
+  Always
+  Hold
+
+GrappleTo
+  Slingshot
+    Force applied to the character for some duration?
+  Vault
+    Abilty to jump when in some window of making contact
+  Attack
+    Ability to attack when in some window of making contact
+
+Animation
+  Windup
+    Turn=false
+    FallSpeed=slow
+    Move=false
+  Hand extended (throw)
+    Turn=false
+    FallSpeed=slow
+    Move=
+  Rocket man for the pull
+    Turn=0
+    FallSpeed=0
+    Move=0
+  Launch
+    Steering?
+
+Can Grapple
+  Out of range
+    grapple point inactive
+    no grapple
+    (Optional) attemped/failed grapple on button push
+
+Camera
+  Camera always tracks player?
+  Target is always in camera shot (assuming it's close enough for max camera range)
+*/
 public class Grapple : Ability {
   [SerializeField] LineRenderer GrappleLine;
   [SerializeField] Timeval ThrowDuration;
@@ -25,7 +64,9 @@ public class Grapple : Ability {
         transform.position,
         Defaults.Instance.GrapplePointLayerMask,
         QueryTriggerInteraction.Collide);
-      var toGrapplePoint = Vector3.Distance(transform.position, grapplePoint.transform.position);
+      var toGrapplePoint = Vector3.Distance(
+        transform.position,
+        grapplePoint.transform.position);
       if (isVisible && toGrapplePoint < toTarget) {
         target = grapplePoint;
         toTarget = toGrapplePoint;
@@ -37,7 +78,10 @@ public class Grapple : Ability {
         float fticks = (float)ThrowDuration.Ticks;
         for (var i = 0; i < ThrowDuration.Ticks; i++) {
           var interpolant = (float)i/fticks;
-          var headPosition = Vector3.Lerp(transform.position, target.transform.position, interpolant);
+          var headPosition = Vector3.Lerp(
+            transform.position,
+            target.transform.position,
+            interpolant);
           GrappleLine.SetPosition(0, transform.position + Vector3.up);
           GrappleLine.SetPosition(1, headPosition);
           target.Sources.Add(transform.position);
