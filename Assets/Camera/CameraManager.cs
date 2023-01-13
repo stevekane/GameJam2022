61863d7bd2651a7e5ee2ea029bf8d73ad2c971aa
Steve Kane <kanesteven@gmail.com>
2023-01-13 16:40:05 -0600

    cleaned up base char prefabs which still had deleted camerainterst components and adjusted all characters camera subject radii

--- Assets/Camera/CameraManager.cs
+++ Assets/Camera/CameraManager.cs
@@ -1,7 +1,6 @@
 using Cinemachine;
 using UnityEngine;
 
-// Reference to TargetGroup. Subjects are added/removed primarily from CameraTargetBox
 public class CameraManager : MonoBehaviour {
   public static CameraManager Instance;
 
