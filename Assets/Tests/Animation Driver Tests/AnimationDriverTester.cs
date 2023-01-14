27d82d4c9e435606158aae5b2832478af8a04f18
Steve Kane <kanesteven@gmail.com>
2023-01-14 13:20:48 -0600

    added exception handling to a test script I am going to throw away very soon

--- Assets/Tests/Animation Driver Tests/AnimationDriverTester.cs
+++ Assets/Tests/Animation Driver Tests/AnimationDriverTester.cs
@@ -11,15 +11,19 @@ public class AnimationDriverTester : MonoBehaviour {
 
   TaskScope scope = new();
   async void Start() {
-    await scope.Repeat(async s => {
-      try {
-        Animator.SetTrigger("Attack");
-        AnimationDriver.Play(s, AnimationConfig);
-        await s.Delay(Timeval.FromSeconds(1));
-      } catch (Exception e) {
-        Debug.LogError(e.Message);
-      }
-    });
+    try {
+      await scope.Repeat(async s => {
+        try {
+          Animator.SetTrigger("Attack");
+          AnimationDriver.Play(s, AnimationConfig);
+          await s.Delay(Timeval.FromSeconds(1));
+        } catch (Exception e) {
+          Debug.LogWarning(e.Message);
+        }
+      });
+    } catch (Exception e) {
+      Debug.LogWarning(e.Message);
+    }
   }
 
   void OnDestroy() {
