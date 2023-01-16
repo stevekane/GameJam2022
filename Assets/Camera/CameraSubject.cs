a82169a239d49bfc6019964ce481f6270513b6c5
Steve Kane <kanesteven@gmail.com>
2023-01-15 21:07:17 -0600

    Base Character Camera Subject simplified to react to OnDeath for spawning ghosts. Added position indicator

--- Assets/Camera/CameraSubject.cs
+++ Assets/Camera/CameraSubject.cs
@@ -1,25 +1,18 @@
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 public class CameraSubject : MonoBehaviour {
   public float Radius = 3;
 
-  bool SpawnGhost = false;
-
   void Awake() {
-    SpawnGhost = true;
     CameraManager.Instance.AddTarget(this);
   }
 
-  #if UNITY_EDITOR
-  void OnApplicationQuit() {
-    SpawnGhost = false;
-  }
-  #endif
-
   void OnDestroy() {
-    CameraManager.Instance.RemoveTarget(this, SpawnGhost);
+    CameraManager.Instance.RemoveTarget(this, false);
+  }
+
+  void OnDeath() {
+    Debug.Log("I died, shoot");
+    CameraManager.Instance.RemoveTarget(this, true);
   }
 }
