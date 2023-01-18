574235a35cab57f3ff2918d1efab2a823d09b83d
Matt Perry <mpcomplete@gmail.com>
2023-01-17 19:25:11 -0500

    Fix null ex in CycleAbility

--- Assets/Scripts/Abilities/CycleAbility.cs
+++ Assets/Scripts/Abilities/CycleAbility.cs
@@ -3,12 +3,14 @@ using System.Threading.Tasks;
 
 public class CycleAbility : Ability {
   public List<Ability> Abilities;
-  int CycleIndex = -1;
+  int CycleIndex = 0;
+  int NextIndex => (CycleIndex + 1) % Abilities.Count;
 
   public override AbilityTag ActiveTags => Abilities[CycleIndex].ActiveTags;
 
+  public override bool CanStart(AbilityMethod func) => Abilities[NextIndex].CanStart(Abilities[NextIndex].MainAction);
   public override async Task MainAction(TaskScope scope) {
-    CycleIndex = (CycleIndex + 1) % Abilities.Count;
+    CycleIndex = NextIndex;
     AbilityMethod method = Abilities[CycleIndex].MainAction;
     await AbilityManager.TryRun(scope, method);
   }
