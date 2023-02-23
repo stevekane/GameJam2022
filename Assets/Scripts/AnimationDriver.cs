0d581d7b68a098d194e966977aecf88a3f84b4d8
Matt Perry <mpcomplete@gmail.com>
2023-02-23 18:22:01 -0500

    AnimationTask renames

--- Assets/Scripts/AnimationDriver.cs
+++ Assets/Scripts/AnimationDriver.cs
@@ -42,48 +42,46 @@ public interface IPlayableTask {
 
 public class AnimationTask : IPlayableTask {
   PlayableGraph Graph;
-  public AnimationJobConfig Animation;
+  public AnimationJobConfig Config;
   AnimationDriver Driver;
-  public AnimationClipPlayable Clip;
   double DesiredSpeed = 1;
+  public AnimationClipPlayable Playable { get; private set; }
   public bool IsRunning { get; private set; } = false;
   public int CurrentFrame { get; private set; } = -1;
   public int CurrentPhase { get; private set; } = 0;
-  public int NumFrames => (int)(Animation.Clip.length*Animation.Clip.frameRate+1);
-  public AnimationEvent[] Phases => Animation.Clip.events;
+  public int NumFrames => (int)(Config.Clip.length*Config.Clip.frameRate+1);
+  public AnimationEvent[] Phases => Config.Clip.events;
   public int NumPhases => Phases.Length+1;
 
-  public AnimationTask(AnimationDriver driver, PlayableGraph graph, AnimationJobConfig animation) {
+  public AnimationTask(AnimationDriver driver, PlayableGraph graph, AnimationJobConfig config) {
     Graph = graph;
     Driver = driver;
-    Animation = animation;
-    Clip = AnimationClipPlayable.Create(Graph, Animation.Clip);
+    Config = config;
+    Playable = AnimationClipPlayable.Create(Graph, Config.Clip);
 
     // TODO: Legacy. Update all old anims and remove this.
-    if (Animation.PhaseDurations.Length == 0 && NumPhases == 1) {
-      Animation.PhaseDurations = new[] { Timeval.FromSeconds(Animation.Clip.length) };
+    if (Config.PhaseDurations.Length == 0 && NumPhases == 1) {
+      Config.PhaseDurations = new[] { Timeval.FromSeconds(Config.Clip.length) };
     }
 
-    Debug.Assert(Animation.PhaseDurations.Length == NumPhases,
-      $"Number of phase durations should match the number of phases (animation events + 1) for clip {animation.Clip}");
+    Debug.Assert(Config.PhaseDurations.Length == NumPhases,
+      $"Number of phase durations should match the number of phases (animation events + 1) for clip {config.Clip}");
     SetPhaseDuration();
   }
 
-  public void SetSpeed(double speed) => DesiredSpeed = speed;
-
   public void Pause() {
-    if (Clip.IsValid())
-      Clip.Pause();
+    if (Playable.IsValid())
+      Playable.Pause();
   }
   public void Resume() {
-    if (Clip.IsValid())
-      Clip.Play();
+    if (Playable.IsValid())
+      Playable.Play();
   }
 
   public TaskFunc WaitPhase(int phase) => s => WaitPhase(s, phase);
   public async Task WaitPhase(TaskScope scope, int phase) {
     var time = GetPhaseEndTime(phase);
-    while (IsRunning && Clip.GetTime() < time)
+    while (IsRunning && Playable.GetTime() < time)
       await scope.Yield();
   }
   public TaskFunc PauseAfterPhase(int phase) => s => PauseAfterPhase(s, phase);
@@ -105,8 +103,8 @@ public class AnimationTask : IPlayableTask {
 
   // TDO: condense start/run?
   public void Start() {
-    Clip.SetSpeed(DesiredSpeed * Animation.Speed * Driver.Speed);
-    Clip.SetDuration(Animation.Clip.length);
+    Playable.SetSpeed(DesiredSpeed * Config.Speed * Driver.Speed);
+    Playable.SetDuration(Config.Clip.length);
     Driver.Connect(this);
     IsRunning = true;
   }
@@ -114,20 +112,20 @@ public class AnimationTask : IPlayableTask {
   public void Stop() {
     IsRunning = false;
     Driver.Disconnect(this);
-    if (Clip.IsValid())
-      Clip.Destroy();
+    if (Playable.IsValid())
+      Playable.Destroy();
   }
 
   float BlendWeight() {
-    var time = Clip.GetTime();
-    var duration = Clip.GetDuration();
+    var time = Playable.GetTime();
+    var duration = Playable.GetDuration();
     var fraction = time/duration;
     // blending out
-    if (Animation.BlendOutFraction > 0 && fraction >= (1-Animation.BlendOutFraction)) {
-      return 1-(float)(fraction-(1-Animation.BlendOutFraction))/Animation.BlendOutFraction;
+    if (Config.BlendOutFraction > 0 && fraction >= (1-Config.BlendOutFraction)) {
+      return 1-(float)(fraction-(1-Config.BlendOutFraction))/Config.BlendOutFraction;
     // blending in
-    } else if (Animation.BlendInFraction > 0 && fraction <= Animation.BlendInFraction) {
-      return (float)fraction/Animation.BlendInFraction;
+    } else if (Config.BlendInFraction > 0 && fraction <= Config.BlendInFraction) {
+      return (float)fraction/Config.BlendInFraction;
     // full blending
     } else {
       return 1;
@@ -136,12 +134,12 @@ public class AnimationTask : IPlayableTask {
 
   public async Task Run(TaskScope scope) {
     try {
-      while (IsRunning && Clip.IsValid()) {
+      while (IsRunning && Playable.IsValid()) {
         UpdateCurrentFrame();
         UpdateCurrentPhase();
         Driver.SetInputWeight(this, BlendWeight());
-        Clip.SetSpeed(DesiredSpeed * Animation.Speed * Driver.Speed);
-        if (Clip.IsDone())
+        Playable.SetSpeed(DesiredSpeed * Config.Speed * Driver.Speed);
+        if (Playable.IsDone())
           break;
         await scope.Yield();
       }
@@ -152,14 +150,14 @@ public class AnimationTask : IPlayableTask {
   }
 
   void UpdateCurrentFrame() {
-    var clip = Clip.GetAnimationClip();
+    var clip = Playable.GetAnimationClip();
     var frames = (clip.length*clip.frameRate+1)/* / Animator.speed*/;
-    var interpolant = (float)(Clip.GetTime()/clip.length);
+    var interpolant = (float)(Playable.GetTime()/clip.length);
     CurrentFrame = (int)Mathf.Lerp(0, frames, interpolant);
   }
   // TODO: This doesn't support looping.
   void UpdateCurrentPhase() {
-    var time = Clip.GetTime();
+    var time = Playable.GetTime();
     var oldPhase = CurrentPhase;
     while (CurrentPhase < Phases.Length && time >= Phases[CurrentPhase].time)
       CurrentPhase++;
@@ -168,9 +166,9 @@ public class AnimationTask : IPlayableTask {
   }
 
   float GetPhaseStartTime(int phase) => phase-1 < 0 ? 0 : Phases[phase-1].time;
-  float GetPhaseEndTime(int phase) => phase < Phases.Length ? Phases[phase].time : Animation.Clip.length;
+  float GetPhaseEndTime(int phase) => phase < Phases.Length ? Phases[phase].time : Config.Clip.length;
   void SetPhaseDuration() {
-    var duration = Animation.PhaseDurations[CurrentPhase];
+    var duration = Config.PhaseDurations[CurrentPhase];
     var clipDuration = GetPhaseEndTime(CurrentPhase) - GetPhaseStartTime(CurrentPhase);
     DesiredSpeed = duration.Seconds == 0f ? float.MaxValue : clipDuration/duration.Seconds;
   }
@@ -357,11 +355,11 @@ public class AnimationDriver : MonoBehaviour {
   }
 
   public void Connect(AnimationTask task) {
-    Debug.Assert(task.Animation.Mask == Defaults.Instance.UpperBodyMask || task.Animation.Mask == null);
-    var slot = task.Animation.Mask == Defaults.Instance.UpperBodyMask ? UpperBodySlot : WholeBodySlot;
+    Debug.Assert(task.Config.Mask == Defaults.Instance.UpperBodyMask || task.Config.Mask == null);
+    var slot = task.Config.Mask == Defaults.Instance.UpperBodyMask ? UpperBodySlot : WholeBodySlot;
     if (slot.CurrentTask != null)
       slot.CurrentTask.Stop();
-    Graph.Connect(task.Clip, 0, slot.AnimationInput, 0, 1f);
+    Graph.Connect(task.Playable, 0, slot.AnimationInput, 0, 1f);
     slot.CurrentTask = task;
   }
 
