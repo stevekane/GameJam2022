34f6ba134f37d1dfc77d5f6be226acceb3e04b83
Matt Perry <mpcomplete@gmail.com>
2023-02-24 16:33:49 -0500

    Split AbilityTrack to fix editor shittiness

--- Assets/Tests/Timeline Customization/AbilityTrackMixer.cs
+++ Assets/Tests/Timeline Customization/AbilityTrackMixer.cs
@@ -2,14 +2,6 @@ using UnityEngine;
 using UnityEngine.Timeline;
 using UnityEngine.Playables;
 
-[TrackClipType(typeof(AbilityClipAsset))]
-[TrackBindingType(typeof(Ability))]
-public class AbilityTrackAsset : TrackAsset {
-  public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount) {
-    return ScriptPlayable<AbilityTrackMixer>.Create(graph, inputCount);
-  }
-}
-
 public class AbilityTrackMixer : PlayableBehaviour {
   public override void ProcessFrame(Playable playable, FrameData info, object playerData) {
     var ability = playerData as Ability;
