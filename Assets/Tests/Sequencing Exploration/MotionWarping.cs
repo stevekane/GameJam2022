8ce0297d1a8d8e7e632456fae80eb68906d849c0
Steve Kane <kanesteven@gmail.com>
2023-03-21 01:16:18 -0500

    refactored a bit of the interpolation code for root motion warping

--- Assets/Tests/Sequencing Exploration/MotionWarping.cs
+++ Assets/Tests/Sequencing Exploration/MotionWarping.cs
@@ -9,8 +9,8 @@ public enum RotationMatch {
 public class MotionWarping : MonoBehaviour {
   [SerializeField] Animator Animator;
   [SerializeField] InputManager InputManager;
+  [SerializeField] int wait;
 
-  public bool active;
   public int frame;
   public int total;
   public RotationMatch RotationMatch;
@@ -39,26 +39,33 @@ public class MotionWarping : MonoBehaviour {
 
   async Task Anim(TaskScope scope) {
     try {
-      active = false;
+      for (var i = 0; i < wait; i++) {
+        await scope.ListenFor(LogicalTimeline.FixedTick);
+      }
       Animator.SetTrigger("Attack");
       for (var i = 0; i < 20; i++) {
         await scope.ListenFor(LogicalTimeline.FixedTick);
       }
-    } finally {
       frame = 0;
       total = 20;
-      active = true;
+      for (var i = 0; i < 20; i++) {
+        await scope.ListenFor(LogicalTimeline.FixedTick);
+      }
+    } finally {
+      frame = total;
     }
   }
 
   // TODO: Do we constrain Y-axis motion? Currently...weird y-translation happens
   // TODO: Do we want this warp function? It seems there are potentially a lot of possible functions
-  Vector3 WarpMotion(Vector3 position, Vector3 target, Vector3 deltaPosition, float fraction) {
-    var warpDelta = (target - position) * fraction;
+  Vector3 WarpMotion(Vector3 position, Vector3 target, Vector3 deltaPosition, int frame, int total) {
+    var fraction = (float)frame/(float)total;
+    var warpDelta = (target-position) / (total-frame);
     return Vector3.Lerp(deltaPosition, warpDelta, fraction);
   }
 
-  Quaternion WarpRotation(Quaternion rotation, Quaternion target, Quaternion deltaRotation, float fraction) {
+  Quaternion WarpRotation(Quaternion rotation, Quaternion target, Quaternion deltaRotation, int frame, int total) {
+    var fraction = (float)frame/(float)total;
     var warpDelta = Quaternion.Slerp(Quaternion.identity, target * Quaternion.Inverse(rotation), fraction);
     var xyzRotation = Quaternion.Slerp(deltaRotation, warpDelta, fraction);
     var xyzEuler = xyzRotation.eulerAngles;
@@ -66,21 +73,19 @@ public class MotionWarping : MonoBehaviour {
   }
 
   void OnAnimatorMove() {
-    if (active) {
-      var fraction = (float)frame/(float)total;
+    if (frame < total) {
       if (RotationMatch == RotationMatch.Target) {
         var targetPosition = target.position + target.TransformVector(targetOffset);
         var targetRotation = target.rotation;
-        transform.position += WarpMotion(transform.position, targetPosition, Animator.deltaPosition, fraction);
-        transform.rotation *= WarpRotation(transform.rotation, targetRotation, Animator.deltaRotation, fraction);
+        transform.position += WarpMotion(transform.position, targetPosition, Animator.deltaPosition, frame, total);
+        transform.rotation *= WarpRotation(transform.rotation, targetRotation, Animator.deltaRotation, frame, total);
       } else {
         var toTarget = (target.position - transform.position).normalized;
         var targetPosition = target.position - toTarget * targetOffset.magnitude;
         var targetRotation = toTarget.magnitude > 0 ? Quaternion.LookRotation(toTarget) : transform.rotation;
-        transform.position += WarpMotion(transform.position, targetPosition, Animator.deltaPosition, fraction);
-        transform.rotation *= WarpRotation(transform.rotation, targetRotation, Animator.deltaRotation, fraction);
+        transform.position += WarpMotion(transform.position, targetPosition, Animator.deltaPosition, frame, total);
+        transform.rotation *= WarpRotation(transform.rotation, targetRotation, Animator.deltaRotation, frame, total);
       }
-      active = frame <= total;
       frame++;
     } else {
       // want to ONLY take portion of rotation in the XZ-plane
