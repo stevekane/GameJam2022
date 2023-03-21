e3cc8bdc942e567798ac2612b46df8e6e052fb6d
Matt Perry <mpcomplete@gmail.com>
2023-03-21 18:50:00 -0400

    SaveLoad for buildings

--- Assets/Building/BuildObject.cs
+++ Assets/Building/BuildObject.cs
@@ -1,7 +1,21 @@
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 public class BuildObject : MonoBehaviour {
   public string Name;
   public Vector2Int Size;
   public bool CanPlaceMultiple = false;
+  public AssetReference Asset;
+
+  class Serialized : ISerializeableAsset {
+    public AssetReference Asset;
+    public Vector3 Position;
+    public Quaternion Rotation;
+    public void Load() {
+      Asset.InstantiateAsync(Position, Rotation);
+    }
+  }
+  public ISerializeableAsset Save() {
+    return new Serialized { Asset = Asset, Position = transform.position, Rotation = transform.rotation };
+  }
 }
