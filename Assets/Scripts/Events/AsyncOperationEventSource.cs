1a05bce967f6b2800d755dbc5384ff51f9614d97
Steve Kane <kanesteven@gmail.com>
2023-03-27 17:02:13 -0500

    EventSources now have an optional priority setting

--- Assets/Scripts/Events/AsyncOperationEventSource.cs
+++ Assets/Scripts/Events/AsyncOperationEventSource.cs
@@ -2,9 +2,10 @@ using System;
 using UnityEngine;
 
 public class AsyncOperationEventSource : IEventSource<AsyncOperation> {
+  public int Priority { get; }
   public AsyncOperation Operation;
   public AsyncOperationEventSource(AsyncOperation operation) => Operation = operation;
-  public void Listen(Action<AsyncOperation> handler) => Operation.completed += handler;
+  public void Listen(Action<AsyncOperation> handler, int priority = 0) => Operation.completed += handler;
   public void Unlisten(Action<AsyncOperation> handler) => Operation.completed -= handler;
   public void Fire(AsyncOperation op) {}
 }
--- Assets/Tests/InputSystem/InputSystemTester.cs
+++ Assets/Tests/InputSystem/InputSystemTester.cs
@@ -1,15 +1,44 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
+[DefaultExecutionOrder(ScriptExecutionGroups.Input+1)]
 public class InputSystemTester : MonoBehaviour {
-  [SerializeField] InputAction.CallbackContext Context;
+  [SerializeField] InputManager InputManager;
+  [SerializeField] InputMappings InputMappings;
 
-  public void OnMove(InputAction.CallbackContext context) {
-    Debug.Log(context);
-    Debug.Log(1+1);
+  public GroundState GroundState;
+  public AirborneState AirborneState;
+  public MonoBehaviour ActiveState;
+  public List<MonoBehaviour> ActiveAbilities;
+  public TaskScope AbilityScope = new();
+
+  public void StopAbility(MonoBehaviour ability) {
+    ActiveAbilities.Remove(ability);
+    ActiveState.enabled = false;
+    ActiveState.enabled = true;
   }
 
-  public void OnJump(InputAction.CallbackContext context) {
-    Debug.Log("Jump");
+  public void StartAbility(MonoBehaviour ability) {
+    ActiveAbilities.Add(ability);
+  }
+
+  public MonoBehaviour SetActiveState(MonoBehaviour state) {
+    if (ActiveState)
+      ActiveState.enabled = false;
+    ActiveState = state;
+    ActiveState.enabled = true;
+    return state;
+  }
+
+  void Start() {
+    GroundState.enabled = false;
+    AirborneState.enabled = false;
+    SetActiveState(GroundState);
+  }
+
+  void OnDestroy() {
+    Destroy(GroundState);
+    Destroy(AirborneState);
+    AbilityScope.Dispose();
   }
 }
