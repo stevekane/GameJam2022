c831808548dfbd946164839a3b2b8fd878975d8c
Matt Perry <mpcomplete@gmail.com>
2023-04-03 14:59:41 -0400

    Better ItemFlow test setup

--- Assets/Building/Items/ItemFlowManager.cs
+++ Assets/Building/Items/ItemFlowManager.cs
@@ -5,7 +5,6 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
-using static UnityEditor.Progress;
 
 public class ItemFlowManager : MonoBehaviour {
   public static ItemFlowManager Instance;
@@ -159,7 +158,7 @@ public class ItemFlowManager : MonoBehaviour {
       EdgeDemands[edge] = amount;
       if (amount > 0)
         edge.From.TryStartCrafting();
-      //Debug.Log($"Edge {edge} has demand {edgeAmounts[edge]}");
+      Debug.Log($"Edge {edge} has demand {edgeAmounts[edge]}");
     }
   }
 
@@ -272,271 +271,58 @@ public class ItemFlowManager : MonoBehaviour {
     MoveItems();
   }
 
-#if false
-  void Awake() { Test6();  }
-
+#if true
+  Machine NewMachine(Recipe recipe, string name) {
+    var go = new GameObject(name);
+    go.SetActive(false);
+    go.AddComponent<Animator>();
+    go.AddComponent<BuildObject>();
+    var m = go.AddComponent<Machine>();
+    m.Recipe = recipe;
+    go.SetActive(true);
+    return m;
+  }
+  Recipe NewRecipe(float craftTime, Recipe.ItemAmount[] inputs, Recipe.ItemAmount output) {
+    var recipe = ScriptableObject.CreateInstance<Recipe>();
+    recipe.Inputs = inputs;
+    recipe.Outputs = new[] { output };
+    recipe.CraftTime = craftTime;
+    return recipe;
+  }
+  Recipe.ItemAmount NewIngred(ItemInfo info, int count) {
+    var amount = new Recipe.ItemAmount {
+      Item = info,
+      Count = count
+    };
+    return amount;
+  }
+  [ContextMenu("Test flow")]
   void Test() {
-    var solver = SolverContext.GetContext();
-    var model = solver.CreateModel();
-
-    var e01 = new Decision(Domain.IntegerNonnegative, "e01");
-    var e02 = new Decision(Domain.IntegerNonnegative, "e02");
-    var e13 = new Decision(Domain.IntegerNonnegative, "e13");
-    var e23 = new Decision(Domain.IntegerNonnegative, "e23");
-    var e34 = new Decision(Domain.IntegerNonnegative, "e34");
-    model.AddDecisions(new[] { e01, e02, e13, e23, e34 });
-
-    model.AddGoal("Goal", GoalKind.Maximize, e34);
-
-    var i = 0;
-    void DoEdge(Decision e, int max) {
-      model.AddConstraint(null, e >= 0);
-      model.AddConstraint(null, e <= max);
-    }
-    DoEdge(e01, 10000);
-    DoEdge(e02, 10000);
-    DoEdge(e13, 1);
-    DoEdge(e23, 1);
-    DoEdge(e34, 2);
-
-    var m1i = new Decision(Domain.IntegerNonnegative, "m1i");
-    var m2i = new Decision(Domain.IntegerNonnegative, "m2i");
-    var m3i = new Decision(Domain.IntegerNonnegative, "m3i");
-    var m1o = new Decision(Domain.IntegerNonnegative, "m1o");
-    var m2o = new Decision(Domain.IntegerNonnegative, "m2o");
-    var m3o = new Decision(Domain.IntegerNonnegative, "m3o");
-    model.AddDecisions(new[] { m1i, m2i, m3i, m1o, m2o, m3o });
-
-    // constraints: edges on inputs
-    model.AddConstraint(null, m1i == e01);
-    model.AddConstraint(null, m2i == e02);
-    model.AddConstraint(null, m3i == e13 + e23);
-    // constraints: edges on outputs
-    model.AddConstraint(null, m1o == e13);
-    model.AddConstraint(null, m2o == e23);
-    model.AddConstraint(null, m3o == e34);
-    // constraints: input/output rates match
-    model.AddConstraint(null, m1i / 2 == m1o / 1);
-    model.AddConstraint(null, m2i / 2 == m2o / 1);
-    model.AddConstraint(null, m3i / 2 == m3o / 1);
-
-    var solution = solver.Solve();
-    Debug.Log($"Test: e01={e01} e02={e02} e13={e13} e23={e23} output = {e34.GetDouble()} sol={solution}");
-
-    solver.ClearModel();
-  }
-
-  async Task Test2() {
-    var solver = SolverContext.GetContext();
-    var model = solver.CreateModel();
-
-    var m1i = new Decision(Domain.IntegerNonnegative, null);
-    var m2i = new Decision(Domain.IntegerNonnegative, null);
-    var m3i = new Decision(Domain.IntegerNonnegative, null);
-    var m1o = new Decision(Domain.IntegerNonnegative, null);
-    var m2o = new Decision(Domain.IntegerNonnegative, null);
-    var m3o = new Decision(Domain.IntegerNonnegative, null);
-    var t = new Decision(Domain.Real, null);
-    model.AddDecisions(new[] { m1i, m2i, m3i, m1o, m2o, m3o, t });
-    model.AddGoal("Goal", GoalKind.Minimize, t);
-
-    model.AddConstraint(null, t >= 0);
-    // constraints: connections
-    model.AddConstraint(null, m1i == 2 * t);
-    model.AddConstraint(null, m2i == 2 * t);
-    model.AddConstraint(null, m3i == m1o + m2o);
-    // constraints: machine input and output rates
-    model.AddConstraint(null, m1i <= 2 * t);
-    model.AddConstraint(null, m1o <= 1 * t);
-    model.AddConstraint(null, m2i <= 2 * t);
-    model.AddConstraint(null, m2o <= 1 * t);
-    model.AddConstraint(null, m3i <= 2 * t);
-    model.AddConstraint(null, m3o <= 1 * t);
-    // constraints: input/output rates match
-    model.AddConstraint(null, m1i / 2 == m1o / 1);
-    model.AddConstraint(null, m2i / 2 == m2o / 1);
-    model.AddConstraint(null, m3i / 2 == m3o / 1);
-    model.AddConstraint(null, m1o == 1);
-
-    var solution = solver.Solve();
-    Debug.Log($"Test2 m1 = {m1i}->{m1o} m2 = {m2i}->{m2o} m3 = {m3i}->{m3o} sol={solution}");
-    solver.ClearModel();
-  }
-
-  void Test3() {
-    object a = new object();
-    object b = new object();
-    Dictionary<(object, object), int> map = new();
-    map[(a, b)] = 1;
-    map[(b, a)] = 2;
-    map[(b, a)]++;
-    Debug.Log($"map: {map[(a, b)]}, {map[(b, a)]}");
-  }
-
-  async Task Test4() {
-    var solver = SolverContext.GetContext();
-    var model = solver.CreateModel();
-
-    var e01 = new Decision(Domain.IntegerNonnegative, "e01");
-    var e02 = new Decision(Domain.IntegerNonnegative, "e02");
-    var e13 = new Decision(Domain.IntegerNonnegative, "e13");
-    var e23 = new Decision(Domain.IntegerNonnegative, "e23");
-    var e34 = new Decision(Domain.IntegerNonnegative, "e34");
-    var t = new Decision(Domain.Real, null);
-    model.AddDecisions(new[] { e01, e02, e13, e23, e34, t });
-    model.AddGoal("Goal", GoalKind.Minimize, t);
-
-    var i = 0;
-    void DoEdge(Decision e, int max) {
-      //model.AddConstraint(null, e >= 0);
-      //model.AddConstraint(null, e <= max);
-    }
-    DoEdge(e01, 10000);
-    DoEdge(e02, 10000);
-    DoEdge(e13, 1);
-    DoEdge(e23, 1);
-    DoEdge(e34, 2);
-
-    var m1i = new Decision(Domain.IntegerNonnegative, "m1i");
-    var m2i = new Decision(Domain.IntegerNonnegative, "m2i");
-    var m3i = new Decision(Domain.IntegerNonnegative, "m3i");
-    var m1o = new Decision(Domain.IntegerNonnegative, "m1o");
-    var m2o = new Decision(Domain.IntegerNonnegative, "m2o");
-    var m3o = new Decision(Domain.IntegerNonnegative, "m3o");
-    model.AddDecisions(new[] { m1i, m2i, m3i, m1o, m2o, m3o });
-
-    model.AddConstraint(null, t >= 0);
-    // constraints: edges on inputs
-    model.AddConstraint(null, m1i == e01);
-    model.AddConstraint(null, m2i == e02);
-    model.AddConstraint(null, m3i == e13 + e23);
-    // constraints: edges on outputs
-    model.AddConstraint(null, m1o == e13);
-    model.AddConstraint(null, m2o == e23);
-    model.AddConstraint(null, m3o == e34);
-    // constraints: machine input and output rates
-    model.AddConstraint(null, m1i <= 2 * t);
-    model.AddConstraint(null, m1o <= 1 * t);
-    model.AddConstraint(null, m2i <= 2 * t);
-    model.AddConstraint(null, m2o <= 1 * t);
-    model.AddConstraint(null, m3i <= 2 * t);
-    model.AddConstraint(null, m3o <= 1 * t);
-    // constraints: input/output rates match
-    model.AddConstraint(null, m1i / 2 == m1o / 1);
-    model.AddConstraint(null, m2i / 2 == m2o / 1);
-    model.AddConstraint(null, m3i / 2 == m3o / 1);
-
-    model.AddConstraint(null, e01 == 2*t);
-    model.AddConstraint(null, e02 == 2*t);
-    model.AddConstraint(null, e34 == 1);
-
-    var solution = solver.Solve();
-    Debug.Log($"Test4 m1 = {m1i}->{m1o} m2 = {m2i}->{m2o} m3 = {m3i}->{m3o} sol={solution}");
-
-    solver.ClearModel();
-  }
-
-  async Task Test5() {
-    var solver = SolverContext.GetContext();
-    var model = solver.CreateModel();
-
-    var eac = new Decision(Domain.Real, null);
-    var ebc = new Decision(Domain.Real, null);
-    var t = new Decision(Domain.Real, null);
-    model.AddDecisions(new[] { eac, ebc, t });
-    model.AddGoal("Goal", GoalKind.Minimize, t);
-
-    var mao = new Decision(Domain.Real, null);
-    var mbo = new Decision(Domain.Real, null);
-    var mci1 = new Decision(Domain.Real, null);
-    var mci2 = new Decision(Domain.Real, null);
-    var mco = new Decision(Domain.Real, null);
-    model.AddDecisions(new[] { mao, mbo, mci1, mci2, mco });
-
-    model.AddConstraint(null, t >= 0);
-    // constraints: edges on outputs
-    model.AddConstraint(null, mao == eac);
-    model.AddConstraint(null, mbo == ebc);
-    // constraints: edges on inputs
-    model.AddConstraint(null, mci1 == eac);
-    model.AddConstraint(null, mci2 == ebc);
-    // constraints: machine input and output rates
-    model.AddConstraint(null, mao <= 1 * t);
-    model.AddConstraint(null, mbo <= 1 * t);
-    model.AddConstraint(null, mci1 <= 1 * t);
-    model.AddConstraint(null, mci2 <= 1 * t);
-    model.AddConstraint(null, mco <= 1 * t);
-    // constraints: input/output rates match
-    model.AddConstraint(null, mci2 / 1 == mci1 / 1);
-    model.AddConstraint(null, mco / 1 == mci1 / 1);
-
-    model.AddConstraint(null, mco >= 1);
-
-    var solution = solver.Solve();
-    Debug.Log($"Test5 ma = {mao} mb = {mbo} mc = {mci1}+{mci2}->{mco} sol={solution}");
-
-    solver.ClearModel();
-  }
-
-  async Task Test6() {
-    var solver = SolverContext.GetContext();
-    solver.ClearModel();
-    var model = solver.CreateModel();
-
-    Decision NewD() { var d = new Decision(Domain.Real, null); model.AddDecision(d); return d; }
-    var t = NewD();
-    model.AddGoal("Goal", GoalKind.Minimize, t);
-
-    (Decision, Decision) NewM() => (NewD(), NewD());
-    var ma = new (Decision, Decision)[] {
-      NewM(),
-      NewM(),
-      NewM(),
-      NewM(),
+    var itemIron = ScriptableObject.CreateInstance<ItemInfo>();
+    var itemGear = ScriptableObject.CreateInstance<ItemInfo>();
+    var itemBelt = ScriptableObject.CreateInstance<ItemInfo>();
+    var recipeMiner = NewRecipe(1f, new Recipe.ItemAmount[0], NewIngred(itemIron, 1));
+    var recipeGear = NewRecipe(1f, new[] { NewIngred(itemIron, 2) }, NewIngred(itemGear, 1));
+    var recipeBelt = NewRecipe(1f, new[] { NewIngred(itemGear, 1), NewIngred(itemIron, 1) }, NewIngred(itemBelt, 1));
+    Machines = new() {
+      NewMachine(recipeMiner, "ma"),
+      NewMachine(recipeMiner, "mb"),
+      NewMachine(recipeGear, "g"),
+      NewMachine(recipeBelt, "belt"),
     };
-    var mb = new (Decision, Decision)[] {
-      NewM(),
-      NewM(),
+    Edges = new() {
+      new() { From = Machines[0], OutputIdx = 0, To = Machines[2], InputIdx = 0 },
+      //new() { From = Machines[1], OutputIdx = 0, To = Machines[2], InputIdx = 0 },
+      new() { From = Machines[0], OutputIdx = 0, To = Machines[3], InputIdx = 1 },
+      //new() { From = Machines[1], OutputIdx = 0, To = Machines[3], InputIdx = 1 },
+      new() { From = Machines[2], OutputIdx = 0, To = Machines[3], InputIdx = 0 },
     };
-    var mc = NewM();
+    PlayerCraftRequests[Machines[3]] = 1;
 
-    var eab0 = new Decision[] { NewD(), NewD(), NewD() };
-    var eab1 = NewD();
-    var eb0c = NewD();
-    var eb1c = NewD();
+    SolveForRequests();
 
-    var arange = new[] { 0, 1, 2, 3 };
-    var brange = new[] { 0, 1 };
-    model.AddConstraint(null, t >= 0);
-    // constraints: edges on outputs
-    model.AddConstraint(null, ma[0].Item2 == eab0[0]);
-    model.AddConstraint(null, ma[1].Item2 == eab0[1]);
-    model.AddConstraint(null, ma[2].Item2 == eab0[2]);
-    model.AddConstraint(null, ma[3].Item2 == eab1);
-    model.AddConstraint(null, mb[0].Item2 == eb0c);
-    model.AddConstraint(null, mb[1].Item2 == eb1c);
-    // constraints: edges on inputs
-    model.AddConstraint(null, mb[0].Item1 == eab0[0] + eab0[1] + eab0[2]);
-    model.AddConstraint(null, mb[1].Item1 == eab1);
-    model.AddConstraint(null, mc.Item1 == eb0c + eb1c);
-    // constraints: machine input and output rates
-    arange.ForEach(i => { model.AddConstraint(null, ma[i].Item2 <= 1 * t); });
-    brange.ForEach(i => { model.AddConstraint(null, mb[i].Item1 <= 3 * t); });
-    brange.ForEach(i => { model.AddConstraint(null, mb[i].Item2 <= 1 * t); });
-    model.AddConstraint(null, mc.Item1 <= 3 * t);
-    model.AddConstraint(null, mc.Item2 <= 1 * t);
-    // constraints: input/output rates match
-    arange.ForEach(i => { model.AddConstraint(null, ma[i].Item1 / 1 == ma[i].Item2 / 1); });
-    brange.ForEach(i => { model.AddConstraint(null, mb[i].Item1 / 3 == mb[i].Item2 / 1); });
-    model.AddConstraint(null, mc.Item1 / 3 == mc.Item2 / 1);
-
-    model.AddConstraint(null, ma[2].Item1 == 0);
-    model.AddConstraint(null, mc.Item2 >= 2);
-
-    var solution = solver.Solve();
-    Debug.Log($"Test6 t={t} ma = {ma[0].Item2}, {ma[1].Item2}, {ma[2].Item2} -> {mb[0].Item1}; ma4 = {ma[3].Item2} -> {mb[1].Item1}; mb = {mb[0].Item2} + {mb[1].Item2} = {mc.Item1}; mco = {mc.Item2}");
-
-    solver.ClearModel();
+    Edges = new();
+    Machines.ForEach(m => DestroyImmediate(m.gameObject));
   }
 #endif
 
