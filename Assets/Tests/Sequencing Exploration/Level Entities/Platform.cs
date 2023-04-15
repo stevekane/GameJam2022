f216fcf181c4e5ca3202e0ee2ca81843fd36af7d
Steve Kane <kanesteven@gmail.com>
2023-04-15 18:06:08 -0500

    working on defining core character requirements

--- Assets/Tests/Sequencing Exploration/Level Entities/Platform.cs
+++ Assets/Tests/Sequencing Exploration/Level Entities/Platform.cs
@@ -9,10 +9,45 @@ public class Platform : MonoBehaviour {
     transform.position += Time.deltaTime * Velocity;
   }
 
+  /*
+  If you are going 100speed and you land on a platform going 0 speed you should
+  continue going 100speed and your steering should need to overcome that inertia.
+
+  If you are going 100speed and you land on a platform going 50 speed you should
+  continue going 100 speed but your reference frame speed is now 50 speed so your
+  steering should need to overcome only 50speed inertia.
+
+  If you are going 100 speed and you land on a platform going -50 speed you should
+  continue going 100 speed but your reference frame speed is now -50 speed and your
+  steering should need to overcome 150 speed inertia.
+
+  In other words, steering is relative to (v_body - v_reference)
+
+
+  What about leaving a platform?
+
+  You are on a platform not moving while the platform moves 50speed.
+  You jump and are now moving 50 speed in a reference frame going 0 speed.
+  You are riding
+  */
+
+  void OnTriggerEnter(Collider c) {
+    if (c.TryGetComponent(out PhysicsMotion physicsMotion)) {
+      physicsMotion.OverrideVelocityXZ(0, 0, 1);
+      physicsMotion.ReferenceFrameVelocity = Velocity;
+    }
+  }
+
   void OnTriggerStay(Collider c) {
-    if (c.TryGetComponent(out DirectMotion directMotion)) {
-      Debug.Log("Got here");
-      directMotion.AddMotion(Velocity * Time.fixedDeltaTime);
+    if (c.TryGetComponent(out PhysicsMotion physicsMotion)) {
+      physicsMotion.ReferenceFrameVelocity = Velocity;
+    }
+  }
+
+  void OnTriggerExit(Collider c) {
+    if (c.TryGetComponent(out PhysicsMotion physicsMotion)) {
+      physicsMotion.AddVelocity(physicsMotion.ReferenceFrameVelocity);
+      physicsMotion.ReferenceFrameVelocity = Vector3.zero;
     }
   }
 }
