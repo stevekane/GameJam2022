f04e3c01969885c76ce073409654c7304eb38de4
Steve Kane <kanesteven@gmail.com>
2023-04-19 18:43:11 -0500

    created fully generic FieldReference and associated PropertyDrawer

--- Assets/Input/Editor/FieldReferencePropertyDrawer.cs
+++ Assets/Input/Editor/FieldReferencePropertyDrawer.cs
@@ -1,21 +1,25 @@
+using System;
 using System.Reflection;
 using UnityEngine;
 using UnityEditor;
 
-public static class FieldReferencePropertyDrawer {
-  public static void Render<T>(Rect position, SerializedProperty property, GUIContent label) {
+[Serializable]
+[CustomPropertyDrawer(typeof(FieldReference), true)]
+public class FieldReferencePropertyDrawer : PropertyDrawer {
+  public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
     var fieldProp = property.FindPropertyRelative("FieldName");
     var targetProp = property.FindPropertyRelative("Target");
     var target = targetProp.objectReferenceValue;
     var dropDownText = fieldProp.stringValue == "" ? "Select method" : fieldProp.stringValue;
     var p1 = new Rect(position); p1.width /= 2;
+    var fieldType = property.boxedValue.GetType().GetProperty("Value", BindingFlags.Instance | BindingFlags.Public).PropertyType;
     EditorGUI.ObjectField(p1, targetProp, GUIContent.none);
     var p2 = new Rect(position); p2.width /= 2; p2.x += p2.width;
     if (target && EditorGUI.DropdownButton(p2, new GUIContent(dropDownText), FocusType.Keyboard)) {
       var menu = new GenericMenu();
       var fields = target.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public);
       foreach (var field in fields) {
-        if (field.FieldType == typeof(T)) {
+        if (field.FieldType == fieldType) {
           menu.AddItem(new GUIContent(field.Name), fieldProp.stringValue == field.Name, delegate {
             fieldProp.stringValue = field.Name;
             property.serializedObject.ApplyModifiedProperties();
--- Assets/Input/FieldReference.cs
+++ Assets/Input/FieldReference.cs
@@ -1,8 +1,10 @@
 using System;
 using UnityEngine;
 
+public class FieldReference {}
+
 [Serializable]
-public class FieldReference<T,F> where T : MonoBehaviour {
+public class FieldReference<T,F> : FieldReference where T : MonoBehaviour {
   public T Target;
   public string FieldName;
   public F Value {
