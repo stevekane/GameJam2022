c6ee5733a27721f9b9d2454907145f22bdecf9eb
Matt Perry <mpcomplete@gmail.com>
2023-04-19 16:29:19 -0400

    Force unity recompile

--- Assets/Building/BuildPlot.cs
+++ Assets/Building/BuildPlot.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(Crafter))]
 [RequireComponent(typeof(BuildObject))]
 public class BuildPlot : MonoBehaviour {
+  // TODO: This doesn't work after loading. Need to store the recipe somewhere.
   public Recipe BuildRecipe { get; set; }
-
   public void TryConstruct() {
     var crafter = GetComponent<Crafter>();
     Debug.Assert(crafter.Recipes.Contains(BuildRecipe));
--- Assets/Building/ItemInfo.cs
+++ Assets/Building/ItemInfo.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using static UnityEditor.Progress;
 
 [CreateAssetMenu(fileName = "Item", menuName = "Crafting/Item")]
 public class ItemInfo : ScriptableObject {
