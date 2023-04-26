12ebfc44686732641f414d692e8a58f64dd20893
Steve Kane <kanesteven@gmail.com>
2023-04-26 18:21:23 -0500

    melee attacks now cancellable via timeline clips. small chgange to semantics of cancel tags on actions from Any to All with nothing being a default

---
+++ Assets/Playables/ModifyFlagsClip.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ModifyFlagsBehavior : TaskBehavior {
+  public AbilityTag Add;
+
+  public override void Setup(Playable playable) {
+    var ability = (SimpleAbility)UserData;
+    ability.Tags.AddFlags(Add);
+  }
+
+  public override void Cleanup(Playable playable) {
+    var ability = (SimpleAbility)UserData;
+    ability.Tags.ClearFlags(Add);
+  }
+}
+
+public class ModifyFlagsClip : PlayableAsset {
+  public AbilityTag Add;
+  public override Playable CreatePlayable(PlayableGraph graph, GameObject owner) {
+    var playable = ScriptPlayable<ModifyFlagsBehavior>.Create(graph);
+    var behavior = playable.GetBehaviour();
+    behavior.Add = Add;
+    return playable;
+  }
+}
