c661a3dabf7c7dbbfdff58332974e2ba00fd3e89
Steve Kane <kanesteven@gmail.com>
2023-05-04 14:27:38 -0500

    added startup code to disable preview mode on app start whgile in the editor to address bug where offset positions are taken relative to the preview state when loading the game. it sadly does not correct the problem. editor settings reloading the scene does however

--- Assets/Playables/PlayableDirectorExtensions.cs
+++ Assets/Playables/PlayableDirectorExtensions.cs
@@ -3,10 +3,40 @@ using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Playables;
 
+#if UNITY_EDITOR
+using System.Reflection;
+using UnityEditor.Timeline;
+public static class TimelineEditorManager {
+  [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+  public static void OnApplicationStartup() {
+    var timelineEditorWindow = TimelineEditorWindow.GetWindow<TimelineEditorWindow>();
+    var timelineEditorWindowType = timelineEditorWindow.GetType();
+    var concreteType = timelineEditorWindow.GetType();
+    if (!timelineEditorWindow)
+      return;
+    var statePropertyName = "state";
+    var bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+    var stateProperty = timelineEditorWindowType.GetProperty(statePropertyName, bindingFlags);
+    if (stateProperty == null) {
+      Debug.LogError($"Could not find {statePropertyName} on TimelineEditorWindow");
+      return;
+    }
+    var state = stateProperty.GetValue(timelineEditorWindow, null);
+    var stateType = state.GetType();
+    var previewModeName = "previewMode";
+    var previewModeProperty = stateType.GetProperty(previewModeName, bindingFlags);
+    if (previewModeProperty == null) {
+      Debug.LogError($"Could not find {previewModeName} on {stateType}");
+      return;
+    }
+    previewModeProperty.SetValue(state, false);
+  }
+}
+#endif
+
 public static class PlayableDirectorExtensions {
   public static async Task PlayTask(this PlayableDirector director, TaskScope scope, LocalTime localTime) {
     try {
-      director.RebuildGraph();
       director.timeUpdateMode = DirectorUpdateMode.Manual;
       director.extrapolationMode = DirectorWrapMode.None;
       director.time = 0;
@@ -20,7 +50,7 @@ public static class PlayableDirectorExtensions {
     } catch (Exception e) {
       Debug.LogError(e.Message);
     } finally {
-      director.playableGraph.Destroy();
+      director.RebuildGraph();
     }
   }
 }
