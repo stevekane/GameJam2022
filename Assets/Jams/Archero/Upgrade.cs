f0e8e152401033e9106507519d97c4fdc68cc370
Matt Perry <mpcomplete@gmail.com>
2023-06-28 12:38:51 -0400

    Upgrade display names

--- Assets/Jams/Archero/Upgrade.cs
+++ Assets/Jams/Archero/Upgrade.cs
@@ -6,12 +6,13 @@ namespace Archero {
   }
 
   public class Upgrade : ScriptableObject {
+    public string DisplayName;
     public int MaxLevel = 18;
     // Adds the upgrade to the acquired or increases its level.
     public virtual UpgradeData Add(Upgrades us) => null;
     // Applies the effect of the current level of this upgrade.
     public virtual void Apply(Upgrades us) { }
     // Returns description data used for the UI.
-    public virtual UpgradeDescription GetDescription(Upgrades us) => new() { Title = name };
+    public virtual UpgradeDescription GetDescription(Upgrades us) => new() { Title = DisplayName };
   }
 }
