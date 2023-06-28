9a6828d6ec8e6dd371f50b7e37eabdcc61e854c5
Steve Kane <kanesteven@gmail.com>
2023-06-28 16:35:39 -0500

    updated damageable to pass along max hp in damage events and added onspawn damage event. HealthBar now has optional text display of current Health

--- Assets/Jams/Archero/Healthbar.cs
+++ Assets/Jams/Archero/Healthbar.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 namespace Archero {
   public class Healthbar : MonoBehaviour {
     [SerializeField] PersonalCamera PersonalCamera;
-    [SerializeField] Attributes Attributes;
     [SerializeField] Slider Slider;
+    [SerializeField] TextMeshProUGUI HealthText;
     [SerializeField] Vector3 Offset;
 
+    void OnSpawn(DamageEvent damageEvent) {
+      Slider.value = (float)damageEvent.Health / damageEvent.MaxHealth;
+      HealthText.text = damageEvent.Health.ToString();
+    }
+
     void OnDamage(DamageEvent damageEvent) {
-      Slider.value = (float)damageEvent.Health / Attributes.GetValue(AttributeTag.Health, 0);
+      Slider.value = (float)damageEvent.Health / damageEvent.MaxHealth;
+      HealthText.text = damageEvent.Health.ToString();
     }
 
     void OnDeath() {
       Slider.gameObject.SetActive(false);
+      HealthText.gameObject.SetActive(false);
     }
 
     void LateUpdate() {
