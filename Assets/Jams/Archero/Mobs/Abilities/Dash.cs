cbe503a069da5b42032b4e534638949914d4392d
Matt Perry <mpcomplete@gmail.com>
2023-06-30 18:09:11 -0400

    ContactHitbox deals damage from mob contact

--- Assets/Jams/Archero/Mobs/Abilities/Dash.cs
+++ Assets/Jams/Archero/Mobs/Abilities/Dash.cs
@@ -13,11 +13,16 @@ namespace Archero {
 
     public override async Task MainAction(TaskScope scope) {
       await scope.Seconds(.25f);
-      var dir = (Target.position - AbilityManager.transform.position).normalized;
-      var duration = DashDistance/DashSpeed;
-      AI.Velocity = DashSpeed * dir;
-      await scope.Seconds(duration);
-      AI.Velocity = Vector3.zero;
+      try {
+        var dir = (Target.position - AbilityManager.transform.position).normalized;
+        var duration = DashDistance/DashSpeed;
+        AI.Velocity = DashSpeed * dir;
+        AbilityManager.GetComponentInChildren<ContactHitbox>().Mode = ContactHitbox.Modes.Active;
+        await scope.Seconds(duration);
+      } finally {
+        AbilityManager.GetComponentInChildren<ContactHitbox>().Mode = ContactHitbox.Modes.Passive;
+        AI.Velocity = Vector3.zero;
+      }
       await scope.Seconds(.5f);
     }
   }
