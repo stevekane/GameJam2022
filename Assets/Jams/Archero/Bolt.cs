d8e35a2e54e99c85879457184d5f31b6049e606e
Matt Perry <mpcomplete@gmail.com>
2023-07-01 18:45:50 -0400

    Bolt follows mobs

--- Assets/Jams/Archero/Bolt.cs
+++ Assets/Jams/Archero/Bolt.cs
@@ -3,15 +3,26 @@ using UnityEngine;
 namespace Archero {
   public class Bolt : MonoBehaviour {
     LineRenderer LineRenderer;
+    Transform Source;
+    Transform Target;
 
     public static void Create(Bolt prefab, Transform source, Mob target) {
       var bolt = Instantiate(prefab, source.position, Quaternion.identity);
+      bolt.Source = source;
+      bolt.Target = target.transform;
       bolt.LineRenderer.SetPosition(0, source.position + Vector3.up);
       bolt.LineRenderer.SetPosition(1, target.transform.position + Vector3.up);
     }
 
     void Awake() {
       this.InitComponent(out LineRenderer);
+    }
+
+    void FixedUpdate() {
+      if (Source)
+        LineRenderer.SetPosition(0, Source.position + Vector3.up);
+      if (Target)
+        LineRenderer.SetPosition(1, Target.transform.position + Vector3.up);
     }
   }
 }
